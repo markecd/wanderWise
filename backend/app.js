aae2bec7d40eb15ae44f1833e9b62435a05be78f
markecd <muricombe.delic@gmail.com>
2024-06-10 00:38:29 +0200

    Spet znorel bom

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,13 @@ const destinacijaRouter = require('./routes/destinacija')
 const nacrtRouter = require('./routes/nacrt')
 const cohereRouter = require('./routes/cohere')
 
+const allowedOrigins = [
+    'http://localhost:5173', // Local development
+    'https://wanderwise-frontend.onrender.com' // Deployed frontend URL
+  ];
+
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: allowedOrigins, 
     credentials: true
   }));
 app.use(cookieParser());
