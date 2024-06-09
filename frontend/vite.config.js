a19fec4a7cce2533073e12cd44d1d223330d2a13
markecd <muricombe.delic@gmail.com>
2024-06-09 19:01:58 +0200

    dockerizacija

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,8 @@ dotenv.config({ path: '../.env' }) // Ensure the correct path to your .env file
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  }
 })
